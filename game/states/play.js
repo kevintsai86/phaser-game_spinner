490631430070a0b0adcd0ac773e077ea21f2ea19
Kevin Tsai <visfarix@icloud.com>
2015-04-17 16:30:45 +0800

    changed spin button/adde display for spin result

--- game/states/play.js
+++ game/states/play.js
@@ -30,13 +30,21 @@
       // this.spinner = new Spinner(this.game.world.centerX, this.game.world.centerY);
       //and adds it to game
       // this.game.add.existing(this.spinner);
-
+      
+      // display text
+      this.dsplyText = this.game.add.text(100, 525, 'Spin the wheel!', {font: '20px Arial', fill: 'white'});
+      //listener for the spin button
       this.button.events.onInputDown.add(this.clickListener, this);
       // this.spin();
     },
     update: function() {
-      if(this.spinner.body.angularVelocity<0.2)
-        this.spinner.body.angularVelocity = 0;
+      if(this.spinner.body.angularVelocity > 0){
+        this.dsplyText.setText('Spinning...');
+        if(this.spinner.body.angularVelocity<0.2){
+          this.spinner.body.angularVelocity = 0;
+          this.dsplyText.setText('The spinner has stopped at ' + this.spinner.body.angle);
+        }
+      } 
     },
     render: function(){
       this.game.debug.spriteInfo(this.spinner, 5, 16);
@@ -46,7 +54,7 @@
       this.spin();
     },
     spin: function(){
-      this.spinner.body.angularVelocity = Math.random()*10 + 15;
+      this.spinner.body.angularVelocity = Math.random()*20 + 18;
     }
   };
   
